1c66a1098cfc9f4e59fcddf44890c58d53db1a64
ungaul <debaux.julien@gmail.com>
2025-03-20 14:30:58 +0900

    Fixing...

---
+++ api/list.js
@@ -0,0 +1,28 @@
+const { google } = require('googleapis');
+
+const SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
+const FOLDER_ID = process.env.GOOGLE_DRIVE_FOLDER_ID;
+
+const auth = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  SCOPES
+);
+const drive = google.drive({ version: 'v3', auth });
+
+module.exports = async (req, res) => {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method not allowed' });
+    return;
+  }
+  try {
+    const response = await drive.files.list({
+      q: `'${FOLDER_ID}' in parents and trashed=false`,
+      fields: 'files(id, name, description, webContentLink, mimeType)'
+    });
+    res.status(200).json({ status: 'success', data: response.data.files });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
